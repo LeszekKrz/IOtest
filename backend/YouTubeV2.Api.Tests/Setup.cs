0f11e9ae4b3429a13bcbfc71b98582535c3a026e
Karol Denst <kdenst@gmail.com>
2023-03-12 21:38:02 +0100

    added tests

---
+++ backend/YouTubeV2.Api.Tests/Setup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Respawn;
+using Respawn.Graph;
+using YouTubeV2.Application;
+
+namespace YouTubeV2.Api.Tests
+{
+    internal class Setup
+    {
+        public static YTContext SetUpContext(string connection)
+        {
+            var dbOption = new DbContextOptionsBuilder<YTContext>()
+                .UseSqlServer(connection)
+                .Options;
+            return new YTContext(dbOption);
+        }
+
+        public static async Task ResetDatabaseAsync(string connection)
+        {
+            var respawner = await Respawner.CreateAsync(connection, new RespawnerOptions
+            {
+                TablesToIgnore = new Table[]
+                    {
+                        "__EFMigrationsHistory",
+                        "AspNetRoles",
+                    }
+            });
+
+            await respawner.ResetAsync(connection);
+        }
+
+        internal static WebApplicationFactory<Program> GetWebApplicationFactory()
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, @"..\..\..\appsettings.test.json");
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment("Test");
+                builder.ConfigureAppConfiguration((_, conf) =>
+                {
+                    conf.AddJsonFile(configPath);
+                });
+                builder.ConfigureServices(services =>
+                {
+                    var context = services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(YTContext));
+                    if (context != null)
+                    {
+                        services.Remove(context);
+                        var options = services.Where(r => (r.ServiceType == typeof(DbContextOptions))
+                          || (r.ServiceType.IsGenericType && r.ServiceType.GetGenericTypeDefinition() == typeof(DbContextOptions<>))).ToArray();
+                        foreach (var option in options)
+                        {
+                            services.Remove(option);
+                        }
+                    }
+
+                    services.AddDbContext<YTContext>(
+                        options => options.UseSqlServer(config.GetConnectionString("Db")));
+                });
+            });
+        }
+    }
+}
---
+++ backend/YouTubeV2.Api.Tests/UserTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using YouTubeV2.Application.DTO;
+using YouTubeV2.Application.Model;
+
+namespace YouTubeV2.Api.Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        private WebApplicationFactory<Program> _webApplicationFactory;
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
+            _webApplicationFactory = Setup.GetWebApplicationFactory();
+            var config = _webApplicationFactory.Services.GetService<IConfiguration>();
+            var connection = config.GetConnectionString("Db");
+            await Setup.ResetDatabaseAsync(connection);
+        }
+
+        [TestMethod]
+        public async Task RegisterShouldAddToDB()
+        {
+            // Arrange
+            var httpClient = _webApplicationFactory.CreateClient();
+
+            var registerDto = new RegisterDto("mail@mail.com", "Senior", "Generator", "Frajdy", "asdf1243@#$GJH");
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(registerDto), Encoding.UTF8, "application/json");
+
+            // Act
+            HttpResponseMessage response = await httpClient.PostAsync("register", stringContent);
+
+            // Assert
+            await _webApplicationFactory.DoWithinScope<UserManager<User>>(
+                async userManager =>
+                {
+                    User userResult = await userManager
+                        .Users
+                        .SingleOrDefaultAsync(x => x.Email == registerDto.email);
+
+                    userResult.Should().NotBeNull();
+                    userResult.Name.Should().Be(registerDto.name);
+                    userResult.Surname.Should().Be(registerDto.surname);
+                    userResult.UserName.Should().Be(registerDto.nickname);
+                    userResult.NormalizedUserName.Should().Be(registerDto.nickname.ToUpper());
+                    userResult.Email.Should().Be(registerDto.email);
+                    userResult.NormalizedUserName.Should().Be(registerDto.nickname.ToUpper());
+                    userManager.PasswordHasher.VerifyHashedPassword(userResult, userResult.PasswordHash, registerDto.password);
+
+                    var roles = await userManager.GetRolesAsync(userResult);
+                    roles.Should().Contain(Role.User);
+                    roles.Should().Contain(Role.Creator);
+                });
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
